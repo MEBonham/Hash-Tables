aca6a83295f6ddc71c124b2d4d6217fe44c79c70
MEBonham <programmer@mckaybonham.com>
2019-03-27 12:03:14 -0600

    Create_hash_table and Hash_table_insert

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -71,6 +71,8 @@ unsigned int hash(char *str, int max)
 BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht;
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair *));
 
   return ht;
 }
@@ -84,7 +86,18 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
+  Pair *p = create_pair(key, value);
 
+  unsigned int index = hash(key, ht->capacity);
+
+  if (*(ht->storage + index))
+  {
+    Pair *old_pair = *(ht->storage + index);
+    printf("WARNING: Overwriting key %s.\n", old_pair->key);
+    destroy_pair(*(ht->storage + index));
+  }
+
+  *(ht->storage + index) = p;
 }
 
 /****
